dd33238d718bbddc875e828d0299c26bbae11af2
Juan Cruz Ferreira <juancruzferreiramonteiro04@gmail.com>
2025-05-24 02:51:36 -0300

    machine_handler - in_progress Main functions done, some adjustments to make

--- src/core/machine_handler.py
+++ src/core/machine_handler.py
@@ -1,29 +1,37 @@
 class MachineHandler: 
     
-    def __init__ (self,x,y,z,f): #lo hago a lo ultimo, hay que considerar los paremtros de inicializacion.
+    def __init__ (self,x=0,y=0,z=0,f=2500,layers=1,layer_thick=1): #Analizar que otros parámetros de inicio y que valores default
         self.x = x
         self.y = y
         self.z = z
+        self.f = f
+        self.layers = layers
+        self.layers_thick = layer_thick
         
     
     def _linear_move (self, start_p, end_p):
         if not ((self.x == start_p.x) and (self.y == start_p.y)):
             self.g_code += f'G0 X{start_p.x} Y{start_p.y} Z{self.z} Y{self.y}\n' #We don't have any line between the last point and the actual point
-        self.g_code += f'G1 X{end_p.x} Y{end_p.y} Z{self.z} F{self.f}\n' #faltaria agregar logica para extruder
+        self.g_code += f'G1 X{end_p.x} Y{end_p.y} Z{self.z} F{self.f}\n' #faltaría agregar lógica para extruder
         self.x, self.y = end_p.x, end_p.y
     
-    def _arc_move (self, start_p, end_p, i, j,value): #podriamos agregar logica para circulo completo con el comando P
+    def _arc_move (self, start_p, end_p, i, j,value): #podríamos agregar lógica para circulo completo con el comando P
         if not ((self.x == start_p.x) and (self.y == start_p.y)):
             self.g_code += f'G0 X{start_p.x} Y{start_p.y} Z{self.z} Y{self.y}\n' #We don't have any line between the last point and the actual point
-        self.g_code += f'G{value} X{end_p.x} Y{end_p.y} Z{self.z} I{i} J{j} F{self.f} \n' #faltaria logica para E
+        self.g_code += f'G{value} X{end_p.x} Y{end_p.y} Z{self.z} I{i} J{j} F{self.f} \n' #faltaría lógica para E
         self.x, self.y = end_p.x, end_p.y
     
-    def _generate_gcode (self, entity_list):
+    def _generate_gcode (self, entity_list,file_name):
+        f = open(file_name, "w")
         for i in range(0,self.layers):
+            f.write(f'Layer {i}'\n)
             self.z = self.layer_thick * i
             for command in entity_list: 
                 if (command['command'] == 'G1'):
                     self._linear_move(self,command['param']['start'],command['param']['end'])
                 elif (command['command'] == 'G2-3'):
                     self._arc_move(self,command['param']['start'],command['param']['end'],command['param']['i'],command['param']['j'],command['param']['value'])
-                    
+            #Agregar algún lógica necesaria para el final de cada layer.
+            f.write(self.g_code)        
+        #Agregar lógica para final de archivo, apagar motores o cosas por el estilo.
+        
