f3dac6687bec4950ef67c3777c4f813ce597a50c
Juan Cruz Ferreira Monteiro <juancruzferreiramonteiro04@gmail.com>
2025-05-26 11:01:24 -0300

    Implantation of the dxf_reader
    Logic to read a dxf file and parse its entity's

---
+++ src/core/dxf_parser.py
@@ -0,0 +1,25 @@
+import ezdxf
+from ezdxf.math import OCS, Matrix44
+from src.core.gcode_generator import GcodeGenerator
+
+def generate_entity_list(filename): #Estudiar bien el modelo coordenado de dxf para poder entender los cambios necesarios. Sobre todo para diferentes layers
+    #Etapa 2
+    try: 
+        doc = ezdxf.readfile(filename)
+    except Exception as e:
+        raise FileError(f'File Error. Please retry. {e}') from e
+    
+    model_space = doc.modelspace()
+    
+    for entity in  model_space:
+        gcode_generator = GcodeGenerator()
+        if (entity.dxftype() == 'LINE'):
+            gcode_generator.line_entity(entity.dxf.start, entity.dxf.end)
+        elif (entity.dxftype() == 'ARC'):
+            gcode_generator.arc_entity(entity.dxf.center, entity.dxf.radius, entity.dxf.start_angle, entity.dxf.end_angle)
+        elif (entity.dxftype() == 'LWPOLYLINE'):
+            gcode_generator.lwpolyline_entity(entity)
+        else: 
+            print(f'No support for entity {entity.deftype()}')
+
+            
